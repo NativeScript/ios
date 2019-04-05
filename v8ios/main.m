8c874e1f3a72f556386b48f580b5b99007e4cb00
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-04-05 11:34:59 +0300

    main.mm -> main.m

---
+++ v8ios/main.m
@@ -0,0 +1,24 @@
+#import <UIKit/UIKit.h>
+#include "Runtime.h"
+#include "Tasks.h"
+
+int main(int argc, char * argv[]) {
+    // TODO: Statically ensure that the UIKit.framework is loaded. This needs to be moved in a SymbolResolver class later
+    // to ensure that the required protocols are dynamically loaded at runtime
+    assert(@protocol(UIApplicationDelegate));
+
+    @autoreleasepool {
+        NSString* resourcePath = [[NSBundle mainBundle] resourcePath];
+        NSArray* components = [NSArray arrayWithObjects:resourcePath, @"app", nil];
+        NSString* path = [NSString pathWithComponents:components];
+
+        tns::Runtime* runtime = new tns::Runtime();
+        std::string baseDir = [path UTF8String];
+        runtime->Init(baseDir);
+        runtime->RunScript("index.js");
+
+        tns::Tasks::Drain();
+
+        return 0;
+    }
+}
