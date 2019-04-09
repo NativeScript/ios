f8e350a5f4724c45bbba9d9ffe1938651e7f5819
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-04-09 16:41:39 +0300

    Updated v8 binaries

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
     OUTFOLDER=outgn/$CURRENT_ARCH-release
     echo "Building for $OUTFOLDER"
-    gn gen $OUTFOLDER --args="v8_use_snapshot=true v8_use_external_startup_data=true use_xcode_clang=true enable_ios_bitcode=true is_debug=false symbol_level=0 v8_enable_i18n_support=false target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" target_os=\"ios\" ios_deployment_target=\"10.0\""
+    gn gen $OUTFOLDER --args="use_custom_libcxx=false symbol_level=0 v8_enable_v8_checks=false v8_enable_debugging_features=false is_debug=false v8_use_snapshot=true v8_use_external_startup_data=true use_xcode_clang=true enable_ios_bitcode=true v8_enable_i18n_support=false target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" target_os=\"ios\" ios_deployment_target=\"10.0\""
     ninja -C $OUTFOLDER ${MODULES[@]}
 
     for MODULE in ${MODULES[@]}
