0354737bb533d8914174b7566529922737627e37
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-04-16 15:24:06 +0300

    Added armv7 libffi build instructions

--- build_libffi.sh
+++ build_libffi.sh
@@ -7,6 +7,13 @@ export CXX="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.x
 make
 ar r ./$ARM64_OUT/libffi.a ./$ARM64_OUT/src/*.o ./$ARM64_OUT/src/aarch64/*.o
 
+#ARM_OUT="armv7-apple-darwin13"
+#export CC="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch armv7 -fembed-bitcode -miphoneos-version-min=10.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.2.sdk"
+#export CXX="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -arch armv7 -fembed-bitcode -miphoneos-version-min=10.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.2.sdk"
+#./configure --disable-shared --host="$ARM_OUT"
+#make
+#ar r ./$ARM_OUT/libffi.a ./$ARM_OUT/src/*.o ./$ARM_OUT/src/arm/*.o
+
 X86_64_OUT="x86_64-apple-darwin13"
 export CC="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch x86_64 -fembed-bitcode-marker -mios-simulator-version-min=10.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator12.2.sdk"
 export CXX="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -arch x86_64 -fembed-bitcode-marker -mios-simulator-version-min=10.0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator12.2.sdk"
@@ -14,4 +21,5 @@ export CXX="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.x
 make
 ar r ./$X86_64_OUT/libffi.a ./$X86_64_OUT/src/*.o ./$X86_64_OUT/src/x86/*.o
 
+#lipo ./$ARM64_OUT/libffi.a ./$ARM_OUT/libffi.a ./$X86_64_OUT/libffi.a -create -output ./libffi.a
 lipo ./$ARM64_OUT/libffi.a ./$X86_64_OUT/libffi.a -create -output ./libffi.a
