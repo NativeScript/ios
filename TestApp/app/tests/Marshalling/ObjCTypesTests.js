751808c1547bcf3c466b76fbcb952a0b0f42607f
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-06-10 18:04:01 +0300

    enable NSTimer unit test

--- TestApp/app/tests/Marshalling/ObjCTypesTests.js
+++ TestApp/app/tests/Marshalling/ObjCTypesTests.js
@@ -228,23 +228,23 @@ describe(module.id, function () {
     //     expect(NSNull.null() instanceof NSObject).toBe(true);
     // });
 
-    // it("Initialize a class(NSTimer) whose init deallocates the result from alloc", function () {
-    //    var actionCalled = false;
+    it("Initialize a class(NSTimer) whose init deallocates the result from alloc", function () {
+       var actionCalled = false;
 
-    //    var JSObject = NSObject.extend({
-    //        action: function () {
-    //            actionCalled = true;
-    //        }
-    //    }, {
-    //        exposedMethods: {
-    //            action: { returns: interop.types.void }
-    //        }
-    //    });
+       var JSObject = NSObject.extend({
+           action: function () {
+               actionCalled = true;
+           }
+       }, {
+           exposedMethods: {
+               action: { returns: interop.types.void }
+           }
+       });
 
-    //    var timer = NSTimer.alloc().initWithFireDateIntervalTargetSelectorUserInfoRepeats(new Date(), 0, new JSObject(), "action", null, true);
-    //    timer.fire();
-    //    expect(actionCalled).toBe(true);
-    // });
+       var timer = NSTimer.alloc().initWithFireDateIntervalTargetSelectorUserInfoRepeats(new Date(), 0, new JSObject(), "action", null, true);
+       timer.fire();
+       expect(actionCalled).toBe(true);
+    });
 
     it("String boxed", function () {
         var string = "hello";
