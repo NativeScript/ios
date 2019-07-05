5d921985b05f29a4202e06d1171d323773865983
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-07-05 17:41:31 +0300

    Updated timers.js

--- TestApp/app/tests/Infrastructure/timers.js
+++ TestApp/app/tests/Infrastructure/timers.js
@@ -1,43 +1,68 @@
-// https://github.com/NativeScript/NativeScript/blob/master/timer/timer.ios.ts
+// https://github.com/NativeScript/NativeScript/blob/master/tns-core-modules/timer/timer.ios.ts
 
-var timeoutCallbacks = {};
+var timeoutCallbacks = new Map();
+var timerId = 0;
 
-var TimerCallbackTarget = NSObject.extend({
-    "tick:": function (timer) {
-        this.callback.call(null);
+var TimerTargetImpl = (function (_super) {
+    __extends(TimerTargetImpl, _super);
+    function TimerTargetImpl() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-}, {
-    exposedMethods: {
-        "tick:": { returns: interop.types.void, params: [ NSTimer ] }
-    }
-});
-
+    TimerTargetImpl.initWithCallback = function (callback, id, shouldRepeat) {
+        var handler = TimerTargetImpl.new();
+        handler.callback = callback;
+        handler.id = id;
+        handler.shouldRepeat = shouldRepeat;
+        return handler;
+    };
+    TimerTargetImpl.prototype.tick = function (timer) {
+        if (!this.disposed) {
+            this.callback();
+        }
+        if (!this.shouldRepeat) {
+            this.unregister();
+        }
+    };
+    TimerTargetImpl.prototype.unregister = function () {
+        if (!this.disposed) {
+            this.disposed = true;
+            var timer = timeoutCallbacks.get(this.id).k;
+            timer.invalidate();
+            timeoutCallbacks.delete(this.id);
+        }
+    };
+    TimerTargetImpl.ObjCExposedMethods = {
+        "tick": { returns: interop.types.void, params: [NSTimer] }
+    };
+    return TimerTargetImpl;
+}(NSObject));
 function createTimerAndGetId(callback, milliseconds, shouldRepeat) {
-    var id = new Date().getUTCMilliseconds();
-
-    var target = new TimerCallbackTarget();
-    target.callback = callback;
-    var timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(milliseconds / 1000, target, "tick:", null, shouldRepeat);
-
-    if (!timeoutCallbacks[id]) {
-        timeoutCallbacks[id] = timer;
-    }
-
+    timerId++;
+    var id = timerId;
+    var timerTarget = TimerTargetImpl.initWithCallback(callback, id, shouldRepeat);
+    var timer = NSTimer.scheduledTimerWithTimeIntervalTargetSelectorUserInfoRepeats(milliseconds / 1000, timerTarget, "tick", null, shouldRepeat);
+    NSRunLoop.currentRunLoop.addTimerForMode(timer, NSRunLoopCommonModes);
+    var pair = { k: timer, v: timerTarget };
+    timeoutCallbacks.set(id, pair);
     return id;
 }
 
 function setTimeout(callback, milliseconds) {
-    if (typeof milliseconds === "undefined") {
-        milliseconds = 0;
+    if (milliseconds === void 0) { milliseconds = 0; }
+    var args = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        args[_i - 2] = arguments[_i];
     }
-    return createTimerAndGetId(callback, milliseconds, false);
+    var invoke = function () { return callback.apply(void 0, args); };
+    return createTimerAndGetId(invoke, milliseconds, false);
 }
-global.setTimeout = setTimeout;
 
 function clearTimeout(id) {
-    if (timeoutCallbacks[id]) {
-        timeoutCallbacks[id].invalidate();
-        timeoutCallbacks[id] = null;
+    var pair = timeoutCallbacks.get(id);
+    if (pair) {
+        pair.v.unregister();
     }
 }
+
+global.setTimeout = setTimeout;
 global.clearTimeout = clearTimeout;
