81547b5dacc75728c69c0a95f8c9fded31616f7a
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-08-01 17:17:26 +0300

    Added inspector module to V8 build script

--- build_v8.sh
+++ build_v8.sh
@@ -4,7 +4,7 @@ pushd v8
 
 ARCH_ARR=(x64 arm64)
 #MODULES=(v8_base_without_compiler v8_compiler v8_libplatform v8_libbase v8_libsampler v8_external_snapshot v8_initializers v8_init torque_generated_initializers torque_generated_definitions)
-MODULES=(v8_base_without_compiler v8_compiler v8_libplatform v8_libbase v8_libsampler v8_external_snapshot v8_initializers v8_init torque_generated_initializers v8_crash_keys)
+MODULES=(v8_base_without_compiler v8_compiler v8_libplatform v8_libbase v8_libsampler v8_external_snapshot v8_initializers v8_init torque_generated_initializers)
 
 for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
@@ -12,7 +12,7 @@ do
     echo "Building for $OUTFOLDER"
     gn gen $OUTFOLDER --args="is_official_build=true use_custom_libcxx=false symbol_level=0 v8_enable_v8_checks=false v8_enable_debugging_features=false is_debug=false v8_use_snapshot=true v8_use_external_startup_data=true use_xcode_clang=true enable_ios_bitcode=true v8_enable_i18n_support=false target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" target_os=\"ios\" ios_deployment_target=\"10.0\""
     #gn gen $OUTFOLDER --args="use_custom_libcxx=false symbol_level=2 v8_enable_v8_checks=true v8_enable_debugging_features=true is_debug=true v8_use_snapshot=true v8_use_external_startup_data=true use_xcode_clang=true enable_ios_bitcode=true v8_enable_i18n_support=false target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" target_os=\"ios\" ios_deployment_target=\"10.0\""
-    ninja -C $OUTFOLDER ${MODULES[@]}
+    ninja -C $OUTFOLDER ${MODULES[@]} inspector
 
     for MODULE in ${MODULES[@]}
     do
@@ -47,5 +47,14 @@ do
     FAT_LIBRARY_OUTPUT="$FAT_LIBRARY_OUTPUT -create -output $DIST/lib$MODULE.a"
     eval $FAT_LIBRARY_OUTPUT
 done
+
+FAT_LIBRARY_OUTPUT="lipo"
+for CURRENT_ARCH in ${ARCH_ARR[@]}
+do
+    OUTFOLDER=out.gn/$CURRENT_ARCH-release
+    FAT_LIBRARY_OUTPUT="$FAT_LIBRARY_OUTPUT $OUTFOLDER/obj/third_party/inspector_protocol/libencoding.a"
+done
+FAT_LIBRARY_OUTPUT="$FAT_LIBRARY_OUTPUT -create -output $DIST/libinspector_protocol.a"
+eval $FAT_LIBRARY_OUTPUT
 
 popd
