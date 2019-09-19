27b24adc41feecd9c21ff39649d159b3b923e66a
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-09-19 17:42:57 +0300

    The build script now produces dSYM

--- build_nativescript.sh
+++ build_nativescript.sh
@@ -11,8 +11,25 @@ echo "Building for ARM64 device"
 xcodebuild -project v8ios.xcodeproj -target "NativeScript" -configuration Release -arch arm64 -sdk iphoneos -quiet
 
 echo "Creating fat library"
-OUTPUT_DIR="dist/NativeScript.framework"
+DIST="dist"
+OUTPUT_DIR="$DIST/NativeScript.framework"
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
+
 cp -r "build/Release-iphoneos/NativeScript.framework/" "$OUTPUT_DIR"
-lipo -create build/Release-iphonesimulator/NativeScript.framework/NativeScript build/Release-iphoneos/NativeScript.framework/NativeScript -output "$OUTPUT_DIR/NativeScript"
+lipo -create \
+    "build/Release-iphonesimulator/NativeScript.framework/NativeScript" \
+    "build/Release-iphoneos/NativeScript.framework/NativeScript" \
+    -output "$OUTPUT_DIR/NativeScript"
+
+DSYM_OUTPUT_DIR="$DIST/NativeScript.framework.dSYM"
+cp -r "build/Release-iphoneos/NativeScript.framework.dSYM/" $DSYM_OUTPUT_DIR
+lipo -create \
+    "build/Release-iphonesimulator/NativeScript.framework.dSYM/Contents/Resources/DWARF/NativeScript" \
+    "build/Release-iphoneos/NativeScript.framework.dSYM/Contents/Resources/DWARF/NativeScript" \
+    -output "$DSYM_OUTPUT_DIR/Contents/Resources/DWARF/NativeScript"
+
+pushd $DIST
+zip -qr "NativeScript.framework.dSYM.zip" "NativeScript.framework.dSYM"
+rm -rf "NativeScript.framework.dSYM"
+popd
