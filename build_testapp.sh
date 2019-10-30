c9d661ea92607c303920e795a52cdb07d4d72706
Darin Dimitrov <darin.dimitrov@gmail.com>
2019-10-30 09:37:47 +0200

    ci: adjust code signing settings

--- build_testapp.sh
+++ build_testapp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-DEV_TEAM=${DEVELOPMENT_TEAM:-}
+NATIVESCRIPT_DEVELOPMENT_TEAM=${DEVELOPMENT_TEAM:-}
+NATIVESCRIPT_CODE_SIGN_IDENTITY=${NATIVESCRIPT_CODE_SIGN_IDENTITY:"iPhone Developer"}
+NATIVESCRIPT_PROVISIONING_PROFILE_SPECIFIER=${NATIVESCRIPT_PROVISIONING_PROFILE_SPECIFIER:-}
 
 echo "Cleanup"
 xcodebuild -project v8ios.xcodeproj -target "TestApp" -configuration Release clean
 
 echo "Building for ARM64 device"
-xcodebuild -project v8ios.xcodeproj -target "TestApp" -configuration Release -arch arm64 -sdk iphoneos -quiet DEVELOPMENT_TEAM=$DEV_TEAM CODE_SIGN_IDENTITY="iPhone Developer"
+xcodebuild -project v8ios.xcodeproj -target "TestApp" -configuration Release -arch arm64 -sdk iphoneos -quiet DEVELOPMENT_TEAM="$NATIVESCRIPT_DEVELOPMENT_TEAM" CODE_SIGN_IDENTITY="$NATIVESCRIPT_CODE_SIGN_IDENTITY" CODE_SIGN_STYLE="Manual" NATIVESCRIPT_PROVISIONING_PROFILE_SPECIFIER="$NATIVESCRIPT_PROVISIONING_PROFILE_SPECIFIER"
 
 (
     set -e;
