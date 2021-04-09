873c2bb46d2a2c4d47db254f7b808d7f696d188c
Igor Randjelovic <rigor789@gmail.com>
2021-04-09 16:38:53 +0200

    feat: drop custom WeakRef implementation & use built-in

--- NativeScript/runtime/WeakRef.h
+++ NativeScript/runtime/WeakRef.h
@@ -8,21 +8,6 @@ namespace tns {
 class WeakRef {
 public:
     static void Init(v8::Local<v8::Context> context);
-private:
-    struct CallbackState {
-        CallbackState(v8::Persistent<v8::Object>* target, v8::Persistent<v8::Object>* holder)
-            : target_(target), holder_(holder) {}
-        v8::Persistent<v8::Object>* target_;
-        v8::Persistent<v8::Object>* holder_;
-    };
-
-    static v8::Local<v8::Function> GetGetterFunction(v8::Isolate* isolate);
-    static v8::Local<v8::Function> GetClearFunction(v8::Isolate* isolate);
-    static void ConstructorCallback(const v8::FunctionCallbackInfo<v8::Value>& info);
-    static void WeakTargetCallback(const v8::WeakCallbackInfo<CallbackState>& data);
-    static void WeakHolderCallback(const v8::WeakCallbackInfo<CallbackState>& data);
-    static void GetCallback(const v8::FunctionCallbackInfo<v8::Value>& info);
-    static void ClearCallback(const v8::FunctionCallbackInfo<v8::Value>& info);
 };
 
 }
