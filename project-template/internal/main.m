c181ec9e2bb494440b1ac652da243647dae3c270
Nathan Walker <walkerrunpdx@gmail.com>
2021-12-29 11:35:07 -0800

    chore: use instance init

--- project-template/internal/main.m
+++ project-template/internal/main.m
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
         config.ArgumentsCount = argc;
         config.Arguments = argv;
 
-        [NativeScript start:config];
+        [[NativeScript alloc] initWithConfig:config];
 
         return 0;
     }
