3b2fc94b76ac0d2e62b2daf8929aedbec2e90206
Eduardo Speroni <edusperoni@gmail.com>
2022-05-11 13:24:29 -0300

    chore: refine metadata generator building and docs

--- build_metadata_generator.sh
+++ build_metadata_generator.sh
@@ -5,17 +5,14 @@ set -e
 
 # try to build in the amount of threads available
 # change this to 1 if you want single threaded builds
-NUMJOBS=$(nproc)
+NUMJOBS=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
 
 function build {
     rm -rf build
     mkdir build
-    pushd "build"
-    cmake -DCMAKE_BUILD_TYPE=Release -DMETADATA_BINARY_ARCH=$1 -DCMAKE_OSX_ARCHITECTURES=$1 ../
-    make clean
-    make -j$NUMJOBS
-    cp ../build-step-metadata-generator.py bin
-    popd
+    cmake -B build -DCMAKE_BUILD_TYPE=Release -DMETADATA_BINARY_ARCH=$1 -DCMAKE_OSX_ARCHITECTURES=$1
+    cmake --build build --target clean
+    cmake --build build -j$NUMJOBS
     mkdir "dist/$1"
     cp -r "build/bin" "dist/$1"
 }
