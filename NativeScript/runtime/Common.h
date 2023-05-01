c2f05555e635d6b1914c20c9f4b44f63a2698e92
⭐caitp⭐ <caitp@users.noreply.github.com>
2023-05-01 16:33:57 -0400

    Support v8::Isolate* as a first parameter to DataWrapper constructor

--- NativeScript/runtime/Common.h
+++ NativeScript/runtime/Common.h
@@ -1,6 +1,8 @@
 #ifndef Common_h
 #define Common_h
 
+#include <type_traits>
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
 #include "v8.h"
@@ -16,7 +18,12 @@ namespace tns {
 
 template <typename T, typename... Args>
 T* MakeGarbageCollected(v8::Isolate* isolate, Args&&... args) {
-    return cppgc::MakeGarbageCollected<T>(ALLOCATION_HANDLE_FOR_ISOLATE(isolate), std::forward<Args>(args)...);
+    if constexpr (std::is_constructible_v<T, v8::Isolate*, Args&&...>) {
+        // Supply the isolate* to the target constructor if it's the first parameter.
+        return cppgc::MakeGarbageCollected<T>(ALLOCATION_HANDLE_FOR_ISOLATE(isolate), isolate, std::forward<Args>(args)...);
+    } else {
+        return cppgc::MakeGarbageCollected<T>(ALLOCATION_HANDLE_FOR_ISOLATE(isolate), std::forward<Args>(args)...);
+    }
 }
 
 }
