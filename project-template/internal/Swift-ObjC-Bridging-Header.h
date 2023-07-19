3f74d10ca816a1b53d78be7f54989a9b5bdfb9dc
Nathan Walker <walkerrunpdx@gmail.com>
2023-07-19 14:32:34 -0700

    chore: project template setup for embed boot setup versatility

--- project-template/internal/Swift-ObjC-Bridging-Header.h
+++ project-template/internal/Swift-ObjC-Bridging-Header.h
@@ -9,3 +9,6 @@
 // Intentionally left blank. Needed for the generation of all Swift classes in
 // the *-Swift.h*.
 // https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/importing_objective-c_into_swift
+
+#import "NativeScript/NativeScript.h"
+#import "NativeScriptStart.h"
