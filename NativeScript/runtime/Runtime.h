11116369009a2fc6bcb99a6261053f39a69a1597
Osei Fortune <fortune.osei@yahoo.com>
2024-01-18 03:32:57 -0400

    feat: URL & URLSearchParams

--- NativeScript/runtime/Runtime.h
+++ NativeScript/runtime/Runtime.h
@@ -69,6 +69,11 @@ private:
     void DefinePerformanceObject(v8::Isolate* isolate, v8::Local<v8::ObjectTemplate> globalTemplate);
     void DefineTimeMethod(v8::Isolate* isolate, v8::Local<v8::ObjectTemplate> globalTemplate);
     void DefineDrainMicrotaskMethod(v8::Isolate* isolate, v8::Local<v8::ObjectTemplate> globalTemplate);
+    
+    void DefineURL(v8::Isolate* isolate, v8::Local<v8::ObjectTemplate> globalTemplate);
+
+    void DefineURLSearchParams(v8::Isolate* isolate, v8::Local<v8::ObjectTemplate> globalTemplate);
+    
     static void PerformanceNowCallback(const v8::FunctionCallbackInfo<v8::Value>& args);
     v8::Isolate* isolate_;
     std::unique_ptr<ModuleInternal> moduleInternal_;
