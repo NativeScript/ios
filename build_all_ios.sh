368a5d1dc592a11ff4e270d4b67b0f27a6fe305e
Nathan Walker <walkerrunpdx@gmail.com>
2024-01-31 15:10:33 -0800

    chore: ignore vision when doing ios build

--- build_all_ios.sh
+++ build_all_ios.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf ./dist
 ./update_version.sh
 ./build_metadata_generator.sh
-./build_nativescript.sh
-./build_tklivesync.sh
+BUILD_VISION=false ./build_nativescript.sh
+BUILD_VISION=false ./build_tklivesync.sh
 ./prepare_dSYMs.sh
 ./build_npm_ios.sh
