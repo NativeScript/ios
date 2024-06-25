c312a10e47d67012c05f9a1c6f4fe1a3343337bb
Eduardo Speroni <edusperoni@gmail.com>
2024-06-25 16:21:49 -0300

    fix: incorrect sed in versioning

--- update_version.sh
+++ update_version.sh
@@ -3,9 +3,9 @@ set -e
 
 # If a parameter is passed to this script then replace the version in package.json by appending $PACKAGE_VERSION
 if [ -n "$1" ]; then
-    sed -i.bak "s/\"version\"[[:space:]]*:[[:space:]]*\"\(.*\)\"[[:space:]]*,/\"version\": \"\1-$PACKAGE_VERSION\",/g" package.json && rm package.json.bak
+    jq ".version = \"$PACKAGE_VERSION\"" package.json > package.json.tmp && rm package.json && mv package.json.tmp package.json
 fi
 
 # Read the version from package.json and replace it inside the NativeScript-Prefix.pch precompiled header
-FULL_VERSION=$(sed -nE "s/.*(\"version\"[[:space:]]*:[[:space:]]*\"(.+)\"[[:space:]]*,).*/\2/p" package.json)
+FULL_VERSION=$(jq -r .version package.json)
 sed -i.bak "s/#define[[:space:]]*NATIVESCRIPT_VERSION[[:space:]]*\"\(.*\)\"/#define NATIVESCRIPT_VERSION \"$FULL_VERSION\"/g" NativeScript/NativeScript-Prefix.pch && rm NativeScript/NativeScript-Prefix.pch.bak
